c7b8f8849260e014a469700c757a623bd2a84d86
siyer13 <sreedharriyer@gmail.com>
2019-08-15 00:46:11 +0530

    Hacker Rank programs  - Updated code  - The code currently get the second highest

--- hacker_rank/nested_list.py
+++ hacker_rank/nested_list.py
@@ -6,18 +6,22 @@ if __name__ == '__main__':
     for _ in range(int(input())):
         name = input()
         score = float(input())
-        name_mark_list = []
-        name_mark_list.append(name)
-        name_mark_list.append(score)
+        name_mark_list = [name, score]
         if lowest == 0.0:
             students.append(name_mark_list)
-            lowest, highest, second_lowest = score
-        elif lowest == score:
-            students.append(name_mark_list)
-        elif lowest > score:
-            students.clear()
             lowest = score
+            second_lowest = score
+            highest = score
+        elif second_lowest == score:
             students.append(name_mark_list)
+        elif score > highest:
+            highest = score
+        elif lowest < score < highest:
+            second_lowest = score
+            students.clear()
+            students.append(name_mark_list)
+        elif lowest < score:
+            lowest = score
 
     print(students)
     stu_name = []
