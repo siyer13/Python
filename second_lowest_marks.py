1af3358f841d99327532f50038bcf66014d690be
Sridhar <sreedharriyer@gmail.com>
2020-08-25 18:20:41 +0530

    Delete second_lowest_marks.py

--- second_lowest_marks.py
+++
@@ -1,7 +0,0 @@
-# https://www.hackerrank.com/challenges/nested-list/problem
-
-if __name__ == '__main__':
-    n = int(input())
-    marksheet = [[input(), float(input())] for _ in range(n)]
-    second_lowest_mark = sorted(list(set([marks for name, marks in marksheet])))[1]
-    print('\n'.join([a for a,b in sorted(marksheet) if b == second_lowest_mark]))
